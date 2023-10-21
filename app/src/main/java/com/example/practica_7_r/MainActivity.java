f1f6c0d70b36161b12d64b18e3c3cc84a3c08581
Arturo <hernandez020610@gmail.com>
2023-10-21 11:30:53 -0600

    Avance chido

--- app/src/main/java/com/example/practica_7_r/MainActivity.java
+++ app/src/main/java/com/example/practica_7_r/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.practica_7_r;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.practica_7_r.adapter.LibroAdapter;
+import com.example.practica_7_r.model.Libro;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Libro> Lista_libros = new ArrayList<>();
+        Lista_libros.add(new Libro("El señor de los anillos", "J.R.R. Tolkien"));
+        Lista_libros.add(new Libro("El hobbit", "J.R.R. Tolkien"));
+        Lista_libros.add(new Libro("El silmarillion", "J.R.R. Tolkien"));
+
+        LibroAdapter la = new LibroAdapter(Lista_libros);
+        RecyclerView rc = findViewById(R.id.rcLibros);
+        rc.setAdapter(la);
+        rc.setLayoutManager(new LinearLayoutManager(this));
+        rc.setHasFixedSize(true);
     }
 }
---
+++ app/src/main/java/com/example/practica_7_r/adapter/LibroAdapter.java
@@ -0,0 +1,60 @@
+package com.example.practica_7_r.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.practica_7_r.R;
+import com.example.practica_7_r.model.Libro;
+
+import java.util.List;
+
+public class LibroAdapter extends RecyclerView.Adapter<LibroAdapter.LibroViewHolder> {
+
+    List<Libro> Lista_libros;
+
+    public LibroAdapter(List<Libro> lista_libros) {
+        Lista_libros = lista_libros;
+    }
+    @NonNull
+    @Override
+    public LibroAdapter.LibroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater ly= LayoutInflater.from(parent.getContext());
+        View v= ly.inflate(0, parent, false);
+        return new LibroViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LibroAdapter.LibroViewHolder holder, int position) {
+        Libro l= Lista_libros.get(position);
+        holder.setData(l);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Lista_libros.size();
+    }
+
+    public class LibroViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTitulo;
+        TextView txtAutor;
+        Libro li;
+
+        public LibroViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitulo= itemView.findViewById(R.id.txtTitulo);
+            txtAutor= itemView.findViewById(R.id.txtAutor);
+        }
+
+        public void setData(Libro l){
+            li = l;
+            txtTitulo.setText(l.getTitulo());
+            txtAutor.setText(l.getAutor());
+        }
+    }
+}

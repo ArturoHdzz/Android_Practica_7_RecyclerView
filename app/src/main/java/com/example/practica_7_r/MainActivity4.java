cac88f0aa974cd7b4e4217f3a87ad86c6162d763
Arturo <hernandez020610@gmail.com>
2023-10-23 22:58:29 -0600

    permisos creo que un poco mejor que lo anterior

--- app/src/main/java/com/example/practica_7_r/MainActivity4.java
+++ app/src/main/java/com/example/practica_7_r/MainActivity4.java
@@ -2,32 +2,47 @@ package com.example.practica_7_r;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        verificar();
+        Button solicitarPermisosButton = findViewById(R.id.solicitarPermisosButton);
+        solicitarPermisosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verificarYContinuar();
+            }
+        });
     }
 
-    private void verificar() {
+    private void verificarYContinuar() {
         int permiso = ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET);
         int Permiso2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if(permiso == PackageManager.PERMISSION_GRANTED && Permiso2 == PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(this, "Tienes permiso", Toast.LENGTH_SHORT).show();
+        if (permiso == PackageManager.PERMISSION_GRANTED && Permiso2 == PackageManager.PERMISSION_GRANTED) {
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
-        }else{
-            requestPermissions(new String[]{android.Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            String[] permisos = {Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "Se necesitan permisos para realizar esta acción", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, permisos, PERMISSION_REQUEST_CODE);
         }
     }
 }

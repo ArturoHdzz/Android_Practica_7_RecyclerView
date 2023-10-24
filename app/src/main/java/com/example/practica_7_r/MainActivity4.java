e024ec070d944a91de76592254497072f852593a
Arturo <hernandez020610@gmail.com>
2023-10-23 23:02:51 -0600

    me falto algo

--- app/src/main/java/com/example/practica_7_r/MainActivity4.java
+++ app/src/main/java/com/example/practica_7_r/MainActivity4.java
@@ -15,22 +15,23 @@ import android.widget.Button;
 public class MainActivity4 extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 1;
+    Button solicitarPermisosButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        Button solicitarPermisosButton = findViewById(R.id.solicitarPermisosButton);
+        solicitarPermisosButton = findViewById(R.id.solicitarPermisosButton);
         solicitarPermisosButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                verificarYContinuar();
+                verificar();
             }
         });
     }
 
-    private void verificarYContinuar() {
+    private void verificar() {
         int permiso = ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET);
         int Permiso2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (permiso == PackageManager.PERMISSION_GRANTED && Permiso2 == PackageManager.PERMISSION_GRANTED) {
